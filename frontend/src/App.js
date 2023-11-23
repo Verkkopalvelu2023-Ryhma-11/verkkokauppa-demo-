5b259f4660f0e2be4a152196792dc21894013d2f
kasperkalliomaa <kasper.kalliomaa@gmail.com>
2023-11-23 13:23:17 +0200

    Lisättiin sivut pages kansioon, alkeellinen routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Boards from './pages/Boards';
+import Fins from './pages/Fins';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Boards" element={<Boards />}></Route>
+        <Route path="/Fins" element={<Fins />}></Route>
+        <Route path="/Cart" element={<Cart />}></Route>
+
+      </Routes>
     </div>
   );
 }
