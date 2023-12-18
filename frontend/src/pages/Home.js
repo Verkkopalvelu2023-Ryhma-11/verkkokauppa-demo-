ba931c98f001bcfc8c9a3a3c543653abffc776a1
kasperkalliomaa <kasper.kalliomaa@gmail.com>
2023-12-18 14:04:13 +0200

    lisäsin linkit etusivun kategoria kuviin

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import Stack from 'react-bootstrap/Stack';
-import finit from '../images/finit_etusivu.png';
-import laudat from '../images/laudat_etusivu.png'
-import Kuva from '../components/etusivunkuva';
+import React from "react";
+import Stack from "react-bootstrap/Stack";
+import finit from "../images/finit_etusivu.png";
+import laudat from "../images/laudat_etusivu.png";
+import Kuva from "../components/etusivunkuva";
 
 export default function Home() {
   return (
-
-
     <div>
-    <Stack gap={3}>
-      <div className="p-2">
-        <h1>SURFFILAUDAT JA FINIT <br></br>CUSTOM MADE IN FINLAND</h1>
+      <Stack gap={3}>
+        <div className="p-2">
+          <h1>
+            SURFFILAUDAT JA FINIT <br></br>CUSTOM MADE IN FINLAND
+          </h1>
         </div>
-      <div className="p-2">
-        <img src={finit} alt='finit' /></div>
-      <div className="p-2"> <img src={laudat} alt='laudat' /></div>
-    </Stack>
-    <Kuva />
+        <div className="p-2">
+          <a href="/Fins">
+
+          <img src={finit} alt="finit" />
+          </a>
+        </div>
+        <div className="p-2">
+          {" "}
+          <a href="/Boards">
+          <img src={laudat} alt="laudat" />
+          </a>
+        </div>
+      </Stack>
+      <Kuva />
     </div>
-   
   );
 }
 // jätin kuva-komponentin tähän, en onnistunut ratkomaan. Muotoilu löytyy app.css .etusivunkuva//
